418d92f207794f2359f78444c6ca2038c041e7a9
JuanCarlos003 <144382179+JuanCarlos003@users.noreply.github.com>
2023-10-17 20:41:02 -0600

    Add files via upload

--- Registro/registro.js
+++ Registro/registro.js
@@ -3,28 +3,22 @@ const usuario = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-
-
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	
 	checkInputs();
 });
-
 function checkInputs() {
 	// trim to remove the whitespaces
 	const usuarioValue = usuario.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const password2Value = password2.value.trim();
-
 	if(usuarioValue === '') {
 		setErrorFor(usuario, 'No se puede dejar el usuario en blanco');
 	} else {
 		setSuccessFor(usuario);
 	}
-
-   
 	if(emailValue === '') {
 		setErrorFor(email, 'No puede dejar el email en blanco');
 	} else if (!isEmail(emailValue)) {
@@ -32,13 +26,32 @@ function checkInputs() {
 	} else {
 		setSuccessFor(email);
 	}
-	
 	if(passwordValue === '') {
 		setErrorFor(password, 'Password no debe ingresar en blanco.');
-	} else {
+	} else if (!isValidPassword(passwordValue)) {
+		setErrorFor(password, 'La contraseña no cumple con los requisitos');
+	  } else {
 		setSuccessFor(password);
 	}
-	
+	function isValidPassword(password) {
+		// validar longitud
+		if (password.length < 8) {
+		  return false;
+		}	  
+		// validar mayúscula
+		if (!/[A-Z]/.test(password)) {
+		  return false;
+		}
+		// validar minúscula
+		if (!/[a-z]/.test(password)) {
+		  return false;
+		}
+		// validar caracter especial
+		if (!/[!@#$%^&*()_+~-]/.test(password)) {
+		  return false;
+		}
+		return true;
+	  }
 	if(password2Value === '') {
 		setErrorFor(password2, 'Password2 no debe ngresar en blanco');
 	} else if(passwordValue !== password2Value) {
@@ -47,32 +60,26 @@ function checkInputs() {
 		setSuccessFor(password2);
 	}
 }
-
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
 	const small = formControl.querySelector('small');
 	formControl.className = 'form-control error';
 	small.innerText = message;
 }
-
 function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
 }
-
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
-
 //----imagen 
 $("#imagen-perfil").ready(function () {
     $("#imagen-perfil-fake").css({"border": "0.1rem solid red"});
     banderaImagen = false;
 });
-
 $("#imagen-perfil").change(function () {
     const archivo = $(this).get(0).files[0];
-
     var allowedExtensions = /(.jpg|.jpeg|.png)$/i;
     if (!allowedExtensions.exec(archivo.name)) {
         alert("Extension de imagen no permitida");
@@ -81,7 +88,6 @@ $("#imagen-perfil").change(function () {
         banderaImagen = false;
         return;
     }
-
     if (archivo == undefined || archivo == null) {
         $("#imagen-perfil-prev").attr("src", "");
         $("#imagen-perfil-fake").css({"border": "0.1rem solid red"});
