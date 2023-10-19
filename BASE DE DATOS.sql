9e2ebb2636b9fec40c4ea0983cba190d9709aabe
Komby12 <126112954+Komby12@users.noreply.github.com>
2023-10-19 16:29:08 -0600

    j

--- BASE DE DATOS.sql
+++ BASE DE DATOS.sql
@@ -10,16 +10,23 @@ CREATE TABLE Usuario(
  Telefono varchar(100) NOT NULL,
  FeNa_Usuario date NOT NULL,
  Correo_Us varchar(100) NOT NULL,
- constraint ID_Pais foreign key(ID_Pais) references Pais (ID_Pais),
- constraint ID_Provincia foreign key(ID_Provincia) references PROVINCIA (ID_Provincia),
- constraint ID_Ciudad foreign key(ID_Ciudad) references Ciudad (ID_Ciudad)
+ ID_Pais INT,
+ ID_Provincia INT,
+ ID_Ciudad INT,
+ foreign key(ID_Pais) references Pais(ID_Pais),
+ foreign key(ID_Provincia) references PROVINCIA (ID_Provincia),
+ foreign key(ID_Ciudad) references Ciudad(ID_Ciudad)
 );
+
+DROP TABLE Producto;
 
 CREATE TABLE Ciudad(
 ID_Ciudad INT PRIMARY KEY NOT NULL,
 Nom_Ciudad VARCHAR(100) NOT NULL,
- constraint ID_Pais foreign key(ID_Pais) references Pais (ID_Pais),
- constraint ID_Provincia foreign key(ID_Provincia) references PROVINCIA (ID_Provincia)
+ID_Pais INT,
+ID_Provincia INT,
+foreign key(ID_Pais) references Pais (ID_Pais),
+foreign key(ID_Provincia) references PROVINCIA (ID_Provincia)
 );
 
 CREATE TABLE PROVINCIA(
@@ -39,7 +46,8 @@ Celu_cliente INT NOT NULL,
 Email_Cliente VARCHAR(100) NOT NULL,
 Fax_Client VARCHAR(100) NOT NULL,
 Boletin_Clien boolean NOT NULL,
-constraint ID_Us foreign key(ID_Us) references Usuario (ID_Us)
+ID_US INT,
+foreign key(ID_Us) references Usuario (ID_Us)
 );
 
 CREATE TABLE Mensaje(
@@ -56,7 +64,8 @@ leido boolean
 CREATE TABLE Operador(
 ID_OP INT PRIMARY KEY NOT NULL,
 TIPO_OP VARCHAR(100),
-constraint ID_Us foreign key(ID_Us) references Usuario (ID_Us)
+ID_US INT,
+foreign key(ID_Us) references Usuario (ID_Us)
 );
 
 CREATE TABLE Pedido(
@@ -71,7 +80,8 @@ Dire_fACTURA VARCHAR(1000) NOT NULL,
 Empresa_Trans VARCHAR(200) NOT NULL,
 NROguia VARCHAR(100) NOT NULL,
 Costo_Trans INT NOT NULL,
-constraint ID_Cliente foreign key(ID_Cliente) references Cliente (ID_Cliente)
+ID_Cliente INT,
+foreign key(ID_Cliente) references Cliente (ID_Cliente)
 );
 
 CREATE TABLE Categoria(
@@ -87,7 +97,8 @@ CREATE TABLE Compra(
 ID_COMPRA INT PRIMARY KEY NOT NULL,
 Fecha_Com DATE NOT NULL,
 total double NOT NULL,
-constraint ID_VEN foreign key(ID_VEN) references VENDEDOR (ID_VEN)
+ID_VEN INT,
+foreign key(ID_VEN) references VENDEDOR (ID_VEN)
 );
 
 
@@ -104,29 +115,33 @@ CONTAC_VEN VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE DETALLE_PEDIDO(
-ID_PEDIDO INT NOT NULL,
+ID_PEDIDODET INT  PRIMARY KEY NOT NULL,
 Item INT NOT NULL,
 CANTIDAD_PE INT NOT NULL,
 PRECIO_PE DOUBLE NOT NULL,
 SUBTOTAL DOUBLE NOT NULL,
 COSTO_ENVIO DOUBLE NOT NULL,
-constraint ID_PEDIDO foreign key(ID_PEDIDO) references Pedido (ID_Pedido),
-constraint ID_PRODCUTO foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO)
+ID_PEDIDO INT,
+ID_PRODUCTO  INT,
+ foreign key(ID_PEDIDO) references Pedido (ID_Pedido),
+ foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO)
 );
 
 CREATE TABLE DETALLE_COMPRA(
-ID_COMPRA INT NOT NULL,
+ID_DETCOMPRA INT NOT NULL,
 ItemCom INT NOT NULL,
 CANTIDAD_COM INT NOT NULL,
 PRECIO_COM DOUBLE NOT NULL,
 SUBTOTAL_COM DOUBLE NOT NULL,
 COSTO_ENVIOCom DOUBLE NOT NULL,
-constraint ID_PRODCUTO foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO),
-constraint ID_COMPRA foreign key(ID_COMPRA) references Compra (ID_COMPRA)
+ID_PRODUCTO INT,
+ID_COMPRA INT,
+foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO),
+foreign key(ID_COMPRA) references Compra (ID_COMPRA)
 );
 
 CREATE TABLE PRODUCTO(
-ID_PRODUCTO INT NOT NULL,
+ID_PRODUCTO INT PRIMARY KEY NOT NULL,
 TITULO VARCHAR(100) NOT NULL,
 DESCRIPCION_PRO VARCHAR(1000) NOT NULL,
 PRECIO_PRO DOUBLE  NOT  NULL,
@@ -137,8 +152,9 @@ MOSTRAR_PORTADA boolean
 
 CREATE TABLE FOTO(
 ID_FOTO INT NOT NULL,
-Path_f  VARCHAR(100),
-constraint ID_PRODCUTO foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO)
+Pat_f  VARCHAR(100),
+ID_PRODUCTO INT,
+foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO)
 );
 
 CREATE TABLE COMENTARIO_PRODUCTO(
@@ -146,9 +162,9 @@ ID_Comment INT NOT NULL,
 TEXTO_COM VARCHAR(200),
 FECHA_COM DATE,
 CONTESTACION VARCHAR(200),
-constraint ID_PRODCUTO foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO),
-constraint ID_Cliente foreign key(ID_Cliente) references Cliente (ID_Cliente),
-constraint ID_OP foreign key(ID_OP) references Operador (ID_OP)
+ foreign key(ID_PRODUCTO) references PRODUCTO (ID_PRODUCTO),
+ foreign key(ID_Cliente) references Cliente (ID_Cliente),
+foreign key(ID_OP) references Operador (ID_OP)
 );
 
 
