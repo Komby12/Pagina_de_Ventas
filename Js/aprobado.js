76693628491ad9d2cc2bd43ba1e7a313e4e57794
Komby12 <126112954+Komby12@users.noreply.github.com>
2023-11-30 20:18:35 -0600

    Cambios
    a

---
+++ Js/aprobado.js
@@ -0,0 +1,129 @@
+const btnDepartamentos = document.getElementById('btn-departamentos'),
+	  btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
+	  grid = document.getElementById('grid'),
+	  contenedorEnlacesNav = document.querySelector('#menu .contenedor-enlaces-nav'),
+	  contenedorSubCategorias = document.querySelector('#grid .contenedor-subcategorias'),
+	  esDispositivoMovil = () => window.innerWidth <= 800;
+
+btnDepartamentos.addEventListener('mouseover', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.add('activo');
+	}
+});
+
+grid.addEventListener('mouseleave', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.remove('activo');
+	}
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('mouseenter', (e) => {
+		if(!esDispositivoMovil()){
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		};
+	});
+});
+
+// EventListeners para dispositivo movil.
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) => {
+	e.preventDefault();
+	if(contenedorEnlacesNav.classList.contains('activo')){
+		contenedorEnlacesNav.classList.remove('activo');
+		document.querySelector('body').style.overflow = 'visible';
+	} else {
+		contenedorEnlacesNav.classList.add('activo');
+		document.querySelector('body').style.overflow = 'hidden';
+	}
+});
+
+// Click en boton de todos los departamentos (Para version movil).
+btnDepartamentos.addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.add('activo');
+	btnCerrarMenu.classList.add('activo');
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.remove('activo');
+	btnCerrarMenu.classList.remove('activo');
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('click', (e) => {
+		if(esDispositivoMovil()){
+			contenedorSubCategorias.classList.add('activo');
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		}
+	});
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelectorAll('#grid .contenedor-subcategorias .btn-regresar').forEach((boton) => {
+	boton.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenedorSubCategorias.classList.remove('activo');
+	});
+});
+
+btnCerrarMenu.addEventListener('click', (e)=> {
+	e.preventDefault();
+	document.querySelectorAll('#menu .activo').forEach((elemento) => {
+		elemento.classList.remove('activo');
+	});
+	document.querySelector('body').style.overflow = 'visible';
+});
+
+const imageSlider = document.getElementById('image-slider');
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+const aprobadoButton = document.getElementById('aprobado-button');
+const desaprobadoButton = document.getElementById('desaprobado-button');
+const images = imageSlider.querySelectorAll('img');
+let currentIndex = 0;
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    if (i === index) {
+      image.style.transform = 'scale(1)';
+    } else {
+      image.style.transform = 'scale(0)';
+    }
+  });
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+prevButton.addEventListener('click', prevImage);
+nextButton.addEventListener('click', nextImage);
+
+aprobadoButton.addEventListener('click', () => {
+  alert('Aprobado');
+});
+
+desaprobadoButton.addEventListener('click', () => {
+  alert('Desaprobado');
+});
+
+// Mostrar la primera imagen al cargar la página
+showImage(currentIndex);
